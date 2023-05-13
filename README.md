f93ab7e7ac4a1a40c205f0c35074c28f25d4a69e
Asanga Perera <asangadinesh777@gmail.com>
2023-05-13 09:31:10 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyGoogleMapLibrary
+
+A description of this package.
---
+++ Sources/MyGoogleMapLibrary/MyGoogleMapLibrary.swift
@@ -0,0 +1,6 @@
+public struct MyGoogleMapLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
