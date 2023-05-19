78be7cef028949fe84b67465c2bea106256b7ec3
Asanga Perera <asangadinesh777@gmail.com>
2023-05-19 04:37:49 -0400

    update

--- Sources/MyGoogleMapLibrary/MyGoogleMapLibrary.swift
+++ Sources/MyGoogleMapLibrary/MyGoogleMapLibrary.swift
@@ -1,10 +1,13 @@
 import GoogleMaps
+import CoreLocation
 
 public struct MyGoogleMapLibrary {
     public private(set) var text = "Hello, World!"
+    var locationManager: CLLocationManager?
 
     public init() {
         GMSServices.provideAPIKey("")
+        locationManager = CLLocationManager()
     }
     
     public func testFunc() {
